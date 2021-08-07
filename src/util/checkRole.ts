81d20c2535b1ed5d42624983ade5fee1046f0c5c
ikechukwu-peter <pitiyke@gmail.com>
2021-08-07 11:55:20 -0700

    feat: role checking for route accessing

--- src/util/checkRole.ts
+++ src/util/checkRole.ts
@@ -1,14 +1,10 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import {  Request, Response, NextFunction } from 'express';
 import userModel from '../models/userModel'
-//@ts-ignore
-import decoder from 'jwt-decode';
-
 
 let checkUser = async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization!.split(" ")[1]!;
-    let data: any = decoder(token);
+    const userId = req.user
     try {
-        let user = await userModel.findById(data.id)
+        let user = await userModel.findById(userId)
         if(!(user!.role === 'client')){
             res.status(403).json({warning:'You are not allowed to access this functionality'})
         }else {
@@ -19,10 +15,9 @@ let checkUser = async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 let checkAdmin = async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization!.split(" ")[1]!;
-    let data: any = decoder(token);
+    const userId = req.user
     try {
-        let user = await userModel.findById(data.id)
+        let user = await userModel.findById(userId)
         if(!(user!.role === 'admin')){
             res.status(403).json({warning:'You are not allowed to access this functionality'})
         }else {
