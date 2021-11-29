5e638932510dc6814bb9d26bbe9d9c35ebb0ec0b
Ikechukwu Peter <60905186+ikechukwu-peter@users.noreply.github.com>
2021-11-29 14:23:59 +0100

    change: error to err

--- src/validation/validator.ts
+++ src/validation/validator.ts
@@ -19,7 +19,7 @@ export const validateUserLogin = [
         const hasError = !error.isEmpty();
 
         if (hasError) {
-            res.status(422).json({ error: error.mapped() });
+            res.status(422).json({ err: error.mapped() });
         } else {
             next();
         }
